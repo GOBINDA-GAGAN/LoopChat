8a4039ae21286c62340618881a07131b3e559c1b
Gobinda Gagan Dey <gobindagagandey@gmail.com>
2025-08-09 23:55:25 +0530

    Update App.jsx
    Home Page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,13 @@
-
-
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 const App = () => {
   return (
-    <div className="bg-red-100">
-      hello world
-    </div>
-  )
-}
+    <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
