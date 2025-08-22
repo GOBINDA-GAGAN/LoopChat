8f7aabc89e519d08306df3d07b15b30387aba20f
Gobinda Gagan Dey <gobindagagandey@gmail.com>
2025-08-22 22:47:17 +0530

    verifyOtp done

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -7,7 +7,7 @@ export const sendOtp = async (req, res) => {
   const { phoneNumber, phoneSuffix, email } = req.body;
 
   const otp = OtpGenerator();
-  const expiry = new Date(Date.now() + 5 * 60 * 1000); 
+  const expiry = new Date(Date.now() + 5 * 60 * 1000);
 
   try {
     let user;
@@ -24,10 +24,11 @@ export const sendOtp = async (req, res) => {
       user.emailOtpExpiry = expiry;
       await user.save();
 
-     
       await sendOtpEmail(email, otp);
 
-      return response(res, 200, "OTP sent successfully to your email", { email });
+      return response(res, 200, "OTP sent successfully to your email", {
+        email,
+      });
     }
 
     // ✅ PHONE-BASED OTP
@@ -52,9 +53,53 @@ export const sendOtp = async (req, res) => {
     return response(res, 200, "OTP sent successfully to your mobile", {
       phoneNumber: fullPhoneNumber,
     });
-
   } catch (error) {
     console.error("Send OTP Error:", error);
+    return response(res, 500, "Internal server error");
+  }
+};
+
+export const verifyOtp = async (req, res) => {
+  const { phoneNumber, phoneSuffix, email, otp } = req.body;
+  try {
+    let user;
+
+    if (email) {
+      user = await User.findOne({ email });
+      if (!user) {
+        return response(res, 404, "User not found");
+      }
+      const now = new Date();
+      if (
+        !user.emailOtp ||
+        String(user.emailOtp) !== String(otp) ||
+        now > new Date(user.emailOtpExpiry)
+      ) {
+        response((res, 400, "Invalid or expired  otp"));
+      }
+      user.isVerified = true;
+      user.emailOtp = null;
+      user.emailOtpExpiry = null;
+      await user.save();
+    } else {
+      if (!phoneNumber || !phoneSuffix) {
+        return response(res, 400, "Phone number and phone suffix are required");
+      }
+      const fullPhoneNumber = `${phoneSuffix}${phoneNumber}`;
+      user = await User.findOne({ phoneNumber });
+      if (!user) {
+        return response(res, 404, "User not found");
+      }
+      const result = await verifyOtp(fullPhoneNumber, otp);
+
+      if (result.status !== "approved") {
+        return response(res, 400, "Invalid otp");
+      }
+      user.isVerified = true;
+      await user.save();
+    }
+  } catch (error) {
+    console.error("verify OTP Error:", error);
     return response(res, 500, "Internal server error");
   }
 };
